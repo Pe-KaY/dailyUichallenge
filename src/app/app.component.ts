6a0df43e289cfbc1abecb28c68704c0d07834b3f
Paa Kwesi Ephraim <peekaylink@gmail.com>
2024-07-23 12:52:35 +0000

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,30 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CardsComponent } from './cards/cards.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CardsComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'challenge';
+  // first
+  img = '../../assets/map_1865269.png';
+  header = 'Allow Location Acccess';
+  description =
+    '  Your Company needs it Personalised recommendations of service providers.  ';
+
+  //second
+  img2 = '../../assets/pushnoti.png';
+  header2 = 'Get Notified';
+  description2 =
+    'Enhance your user experience by ensuring a seamless flow through the activation of notifications';
+
+  // third
+  img3 = '../../assets/done.png';
+  header3 = 'Help us to improve the service';
+  description3 =
+    'In order to provide a better service we would like to collect your data for research purposes, you can decline it anytime!  ';
 }
